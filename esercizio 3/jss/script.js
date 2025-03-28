24eea00eddcf99f2dc0b78f45bc7518f64e51b88
William05112 <liamscervino@gmail.com>
2025-03-28 02:25:00 +0100

    feat: esercizio 3 completato

--- esercizio 3/jss/script.js
+++ esercizio 3/jss/script.js
@@ -1,1 +1,37 @@
-alert(`Hola`)
+const animali = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'elefante', famiglia: 'elefantidi', classe: 'mammiferi' },
+    { nome: 'giraffa', famiglia: 'giraffidi', classe: 'mammiferi' },
+    { nome: 'aquila', famiglia: 'accipitridi', classe: 'uccelli' },
+    { nome: 'serpente', famiglia: 'colubridi', classe: 'rettili' },
+    { nome: 'salmone', famiglia: 'salmonidi', classe: 'pesci' }
+  ];
+  
+  console.log(animali);
+  
+
+//   const mammiferi = [];
+
+//     for (let i = 0; i < animali.length; i++) {
+//         if (animali[i].classe === 'mammiferi') {
+//             mammiferi.push(animali[i]);
+//         }
+
+//     }
+//     console.log(mammiferi);
+
+
+function filtraPerClase (array, classe) {
+    const risultato = [];
+ for (let i = 0; i < array.length; i++) {
+    if (array[i].classe === classe) {
+        risultato.push(array[i]);
+    }
+    
+ }
+    return risultato;
+}
+    const mammiferi = filtraPerClase(animali, 'mammiferi');
+    console.log(mammiferi);
+    
